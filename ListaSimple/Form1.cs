d05e699cbfefe046e5964d2a4af93ff22c36c242
Lizbeth Peña Torres <89703185+Lizbethpt@users.noreply.github.com>
2022-10-03 16:35:54 -0500

    Add files via upload

---
+++ ListaSimple/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Nodo n;
+        Operaciones MiLista = new Operaciones();
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            n = new Nodo();
+            n.Dato = int.Parse(txtNodo.Text);
+            MiLista.Agregar(n);
+            lblLista.Text = MiLista.ToString();
+            txtNodo.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int dato = int.Parse(txtNodo.Text);
+            MiLista.Borrar(dato);
+            lblLista.Text = MiLista.ToString();
+            txtNodo.Clear();
+        }
+    }
+}
---
+++ ListaSimple/Nodo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSimple
+{
+    class Nodo
+    {
+        private int dato;
+        private Nodo siguiente;
+
+        //Propiedades
+        public int Dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+        public Nodo Siguiente
+        {
+            get { return siguiente; }
+            set { siguiente = value; }
+        }
+
+        //Constructor
+        public Nodo()
+        {
+            dato = 0;
+            siguiente = null;
+        }
+
+        public Nodo(int dato, Nodo siguiente)
+        {
+            this.dato = dato;
+            this.siguiente = siguiente;
+        }
+
+        public override string ToString()
+        {
+            return dato + "";
+        }
+    }
+}
